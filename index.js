d2cd27bcb0a0a24b0aa840025addbc3984971546
nisha311003 <nisha311003@gmail.com>
2025-04-20 10:57:36 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ mongoose.connect(process.env.MONGO_URI,{
 app.use('/api', authRoutes);
 app.use('/api', portfolioRoutes);
 
+app.get('/', (req, res) => {
+    res.send('🚀 Backend is running successfully!');
+  })
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
